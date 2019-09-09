16dcc1b7aefea29e65b46e3f220be754b05a3cb4
Barry.Fearon <barryfearon@gmail.com>
2019-09-09 16:39:08 +0100

    snap-scroll

---
+++ resources/views/partials/page-header.blade.php
@@ -0,0 +1,3 @@
+<div class="page-header d-flex justify-content-center align-items-center w-100">
+  <h1>{!! App::title() !!}</h1>
+</div>
---
+++ resources/views/template-fullpage-scroll.blade.php
@@ -0,0 +1,23 @@
+{{--
+  Template Name: Full Page Video
+--}}
+
+@extends('layouts.app')
+@include('partials.page-header')
+<div class="scroll-snap">
+  <section>
+    <h2>Section One</h2>
+  </section>
+  <section>
+    <h2>Section Two</h2>
+  </section>
+  <section>
+    <h2>Section Three</h2>
+  </section>
+  <section>
+    <h2>Section Four</h2>
+  </section>
+  <section>
+    <h2>Section Five</h2>
+  </section>
+</div>
